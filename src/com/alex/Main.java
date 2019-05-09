5f38d5d36b49a82f7d7b1b6a87067e126c23765f
Leo <stbirb@yandex.ru>
2019-05-09 15:53:32 +0300

    Initial commit

---
+++ src/com/alex/Main.java
@@ -0,0 +1,13 @@
+package com.alex;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public static void printSquareStar(int number){
+
+
+
+    }
+}
